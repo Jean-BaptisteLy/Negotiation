808332d5427c9c4b447d50d3fb5161886d955f17
pam <ana.osuna_vargas@etu.upmc.fr>
2021-01-22 21:44:59 +0100

    [add] partition maker and pareto_non_dominated

---
+++ src/utils.py
@@ -0,0 +1,34 @@
+from itertools import *
+
+def partition(tasks):
+    """
+    Cacule toutes les partitions possibles étant donné un ensemble de tâches.
+    """
+    l1 = []
+    l2 = []
+    for pattern in product([True,False],repeat=len(tasks)):
+        l1.append(tuple([x[1] for x in zip(pattern,tasks) if x[0]]))
+        l2.append(tuple([x[1] for x in zip(pattern,tasks) if not x[0]]))
+
+    return list(zip(l1,l2))
+
+
+def non_dominated_po(partitions):
+    """
+    Étant donné un dictionnaire de partitions qui nous permet de connaître la valeur
+    de chaque partition, on fait le traitement des partitions de manière à enlèver celles
+    qui sont Pareto-dominées.
+    """
+    # List of pareto-optimal points
+    po = []
+    for part, ut in partitions_utility.items():
+        dominated = False
+        # We add the task only if it is not dominated
+        for other_part, other_ut in partitions_utility.items():
+            if other_ut[0] > ut[0] and other_ut[1] > ut[1] and other_part != part:
+                dominated = True
+                continue
+        if not dominated:
+            po.append(part)
+        
+    return po
