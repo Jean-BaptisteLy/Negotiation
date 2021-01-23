ece030c528d05ce6827181a9a5dce1d356d8448e
pam <ana.osuna_vargas@etu.upmc.fr>
2021-01-23 19:42:16 +0100

    [update] corrected non_dominated_pareto

--- src/resolution.py
+++ src/resolution.py
@@ -31,21 +31,19 @@ def partition(tasks):
 def non_dominated_po(partitions):
     """
     Étant donné un dictionnaire de partitions qui nous permet de connaître la valeur
-    de chaque partition, on fait le traitement des partitions de manière à enlever celles
+    de chaque partition, on fait le traitement des partitions de manière à enlèver celles
     qui sont Pareto-dominées.
     """
     # List of pareto-optimal points
-    #po = []
     po = {}
     for part, ut in partitions.items():
         dominated = False
         # We add the task only if it is not dominated
         for other_part, other_ut in partitions.items():
-            if other_ut[0] >= ut[0] and other_ut[1] >= ut[1] and other_part != part:
+            if (other_ut[0] > ut[0] and other_ut[1] > ut[1] and other_part != part) or (other_ut[0] > ut[0] and other_ut[1] == ut[1] and other_part != part) or (other_ut[0] == ut[0] and other_ut[1] > ut[1] and other_part != part) or (ut in po.values()):
                 dominated = True
                 continue
         if not dominated:
-            #po.append(part)
             po[part] = ut
         
     return po
